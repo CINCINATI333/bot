133b290529b0432b9e65c30ef332078e5b0f75c2
CINCINATI333 <51190223+CINCINATI333@users.noreply.github.com>
2019-10-14 02:36:46 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,19 +2,6 @@ import vk_api
 import random
 import time
 
-'''
-vk = vk_api.VkApi(token="b0dc785bab967bfe0c04563e5648bc422c1ae6ceeba4341f16d386616ff73365afe85bd7e513eebde0954")
-while True:
-messages = vk.method("messages.getConversations", {"offset": 0, "count": 20, "filter": "unread"})
-    if messages["count"] >= 1:
-        id = messages["items"][0]["last_message"]["text"]
-        body = messages["items"][0]["last_message"]["text"]
-        if body.lower() == "омаева му":
-            vk.method("messages.send", {"peer_id":"id", "message":"шиндейру!"})
-        else:
-            vk.method("messages.send", {"peer_id":"id", "message":"NANI!!!"})
-'''
-
 def get_rand():
   return random.random()*2*9223372036854775806 - 9223372036854775806
 
