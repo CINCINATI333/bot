39d59431512b72567a6141e755a43881f170f24d
CINCINATI333 <51190223+CINCINATI333@users.noreply.github.com>
2019-10-16 14:09:15 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -16,9 +16,8 @@ while True:
     if messages and messages["count"]:
         id = messages["items"][0]["conversation"]["peer"]["id"]
         text = messages["items"][0]["last_message"]["text"]
+	      print('name')
         print(text)
-        user = [ name: _name, surname: _surname ]
-        print(_name)
         d = open('data.txt','a')
         d.write(text + '\n')
         d.close()
@@ -27,5 +26,4 @@ while True:
         else:
             vk.method("messages.send", {"peer_id": id, "message":"Nein!!!!", "random_id": get_rand()})   
     time.sleep( 1 )
-
 
